9e53ddaf9fbfa24b48046d6599a346a2ee35d989
Özkan Deniz <ozkandnz09@gmail.com>
2026-06-05 01:34:45 +0200

    app.get

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,18 @@ const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body; // user will send this data
 
@@ -36,6 +48,7 @@ app.delete("/api/products/:id", async (req, res) => {
     await Product.findByIdAndDelete(id);
     res.status(201).json({ success: true, message: "Product deleted" });
   } catch (error) {
+    console.log("error in deleting products:", error.message);
     res.status(404).json({ success: false, message: " Product not found" });
   }
 });
