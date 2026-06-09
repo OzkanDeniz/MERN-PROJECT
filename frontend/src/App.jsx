5d85efcffdb73eb0a0cd6d2b493ada879c5ff2e0
Özkan Deniz <ozkandnz09@gmail.com>
2026-06-10 01:09:55 +0200

    navbar, homepage, createpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
-    </>
+    <Box minH={"100vh"}>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
