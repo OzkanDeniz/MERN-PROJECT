4776f74e25e25fdf6643ded4245d908aa16319ff
Özkan Deniz <ozkandnz09@gmail.com>
2026-07-29 00:22:49 +0200

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,43 @@
-import { Center, Container, Flex } from "@chakra-ui/react";
+import {
+  Center,
+  Container,
+  Flex,
+  Link,
+  HStack,
+  Button,
+  PlusSquareIcon,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
     <Container maxW={"1140px"} px={2}>
       <Flex
-      h={16}
-      alignItems={Center}
-      justifyContent={"space-between"}
+        h={16}
+        alignItems={Center}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
       >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
 
+        <HStack spacing={2} alignItems={Center}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon />
+            </Button>
+          </Link>
+        </HStack>
       </Flex>
     </Container>
   );
