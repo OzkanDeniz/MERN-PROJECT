6c82330ab4fa9e03f207525035cd0b1d04f11e60
Özkan Deniz <ozkandnz09@gmail.com>
2026-08-07 01:36:42 +0200

    createpage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { Container, Vstack, Heading } from "@chakra-ui/react";
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  return (
+    <Container maxW={"container.sm"}>
+      <Vstack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+      </Vstack>
+    </Container>
+  );
 };
 
 export default CreatePage;
