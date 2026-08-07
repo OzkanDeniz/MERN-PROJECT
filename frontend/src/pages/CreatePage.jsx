1165331953038aca77302d7150ab4b21962b5f44
Özkan Deniz <ozkandnz09@gmail.com>
2026-08-07 02:04:20 +0200

    createpage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Container, Vstack, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Vstack,
+  Heading,
+  Box,
+  useColorModeValue,
+  Input,
+} from "@chakra-ui/react";
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -13,6 +20,42 @@ const CreatePage = () => {
         <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
           Create New Product
         </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <Vstack spacing={4}>
+            <Input
+              placeholder="Poduct Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            ></Input>
+
+            <Input
+              placeholder="Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            ></Input>
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            ></Input>
+          </Vstack>
+        </Box>
       </Vstack>
     </Container>
   );
